ea3168bc91195695975e9bb49a1ca3f2e18d86fe
ethanAthompson <ethan.a.thompson@outlook.com>
2023-11-13 02:46:53 -0500

    new update!

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,10 +1,20 @@
-use axum::Router;
+use axum::{
+    response::{Html, IntoResponse},
+    routing::get,
+    Router,
+};
 use server::{catfacts::routes_catfacts, catimages::routes_catimages};
 use std::net::SocketAddr;
+use tower_http::services::{ServeDir, ServeFile};
 
 #[tokio::main]
 async fn main() {
+    let serve_html = ServeDir::new("public").not_found_service(ServeFile::new("public/index.html"));
     let app = Router::new()
+        // Displays index.html as soon as you load the backend
+        .nest_service("/public", serve_html.clone())
+        .fallback_service(serve_html)
+        // These are the other routes that i've merged
         .merge(routes_catfacts())
         .merge(routes_catimages());
 
---
+++ server/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["public/index.html", "src/**/*.{html,rs}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
